6532fff0aec1e3b4a130a3683beb253a819322f3
Youngpro95 <mmyoung95@naver.com>
2022-08-25 21:13:09 +0900

    inital commit

---
+++ mini-news/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//단순 네비게이션바 구현
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
---
+++ mini-news/src/components/Search/SearchPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchHistory from "./SearchHistory";
+
+//검색 기능 구현 해주시면 될것같습니다.
+
+export default function SearchPage() {
+  return (
+    <div>SearchPage</div>
+  )
+}
