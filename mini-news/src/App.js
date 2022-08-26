7272fcc2da62f44cc2b1f03c618757b4b9074fcd
Youngpro95 <mmyoung95@naver.com>
2022-08-27 02:37:27 +0900

    style : 라우팅을 위한 App.js 수정

--- mini-news/src/App.js
+++ mini-news/src/App.js
@@ -1,15 +1,18 @@
 import NewsListPage from "./components/NewsList/NewsListPage";
 import GlobalStyle from "./styles/GlobalStyle";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <GlobalStyle/>
-      {/* <NavBar/>
-      <SearchBar/>
-      <Clip/>
-      <NewsListPage/> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <GlobalStyle />
+        {/* <NavBar/>
+        <SearchBar/>
+        <Clip/>
+        <NewsListPage/> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
