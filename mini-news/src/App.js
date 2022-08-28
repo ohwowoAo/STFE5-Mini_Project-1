29c8410097ef70e50997b4389fb2e453d0c07ab5
Youngpro95 <mmyoung95@naver.com>
2022-08-29 02:20:09 +0900

    styles : input창 생성

--- mini-news/src/App.js
+++ mini-news/src/App.js
@@ -1,16 +1,22 @@
 import NewsListPage from "./components/NewsList/NewsListPage";
 import GlobalStyle from "./styles/GlobalStyle";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import SearchPage from "./components/Search/SearchPage";
+import SearchHistory from "./components/Search/SearchHistory";
+import ClipPage from "./components/Clip/ClipPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
+    // <Route path="/" element={<Header />}/>
+    <>
+      {/* <Header/> */}
       <Routes>
-        <Route path="/" element={<Header/>}/>
+        <Route path="/" element={<NewsListPage />}>
+          <Route path="/clip"  element={<ClipPage/>}/>
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
