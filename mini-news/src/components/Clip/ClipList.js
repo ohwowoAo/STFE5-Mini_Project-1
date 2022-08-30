94ad391bd1729f9e0f7303cf86406f60a9330612
EJ <sbjdnm.gmail.com>
2022-08-30 17:48:44 +0900

    Feat : Add Clip Page

---
+++ mini-news/src/components/Clip/ClipList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClipPage from './ClipPage'
+
+export default function ClipList({props}) {
+
+    return (
+      <>
+        <ClipPage props={true}/>
+        <div>하하</div>
+      </>
+    )
+  }
